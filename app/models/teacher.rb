84c6d8b062ea4ec43cb499e8d45e5fefdc58f57d
dfm555 <dfm556@gmail.com>
2016-08-06 23:18:25 -0500

    added validates teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -18,4 +18,12 @@
 class Teacher < ActiveRecord::Base
   has_and_belongs_to_many :careers
   has_and_belongs_to_many :subjects
+
+  validates :code, presence: true, uniqueness: true
+  validates :identification, presence: true, uniqueness: true
+  validates :full_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates :rol_id, presence: true
+
 end
